15046301fd6b659a7dce7e1959ea2bc2c291fbb3
Ciaran Maloy <1ciaran.maloy@gmail.com>
2025-01-23 20:26:27 +0000

    some code to make sure that the h5 files are being made and arent to large

--- audioautoencoder/data.py
+++ audioautoencoder/data.py
@@ -499,113 +499,130 @@ def process_and_save_noisy_dataset(
       process_pool=True, 
       manual_checkpoint=None, 
       SNRdB=None, 
-      verbose=False):
+      verbose=False, 
+      checkpoint_file_size=100, 
+      max_file_size_gb=60):
     """
     Process all .wav files in a folder and save the input-output image pairs.
     use this to train a denoising autoencoder 
     This should be used within an if __name__ == '__main__' function for speed
     """
     try:
-      # Get the list of .wav files in the directory
-      print('Gathering wav files....')
-      wav_files = get_all_wav_files(data_dir)
-      total_files = len(wav_files)
-      if total_files == 0:
-          print("No .wav files found in the specified directory.")
-          return
+      random.seed(42)
+      LOGIC = True
+      max_file_size_bytes = max_file_size_gb * 1024**3
 
-      # get noise files
-      print('Gathering noise files....')
-      noise_wav_files = get_all_wav_files(noise_data_dir)
-      total_noise_files = len(noise_wav_files)
-      if total_noise_files == 0:
-          print("No .wav files found in the specified directory.")
-          return
+      while LOGIC:
+        # Get the list of .wav files in the directory
+        print('Gathering wav files....')
+        wav_files = get_all_wav_files(data_dir)
+        random.shuffle(wav_files)
+        total_files = len(wav_files)
+        if total_files == 0:
+            print("No .wav files found in the specified directory.")
+            return
 
-      # Load checkpoint to resume processing
-      if manual_checkpoint is not None:
-        start_batch_idx = manual_checkpoint
-      else:
-        start_batch_idx = load_checkpoint(checkpoint_file)
-      print(f"Resuming from batch index: {start_batch_idx}")
+        # get noise files
+        print('Gathering noise files....')
+        noise_wav_files = get_all_wav_files(noise_data_dir)
+        random.shuffle(noise_wav_files)
+        total_noise_files = len(noise_wav_files)
+        if total_noise_files == 0:
+            print("No .wav files found in the specified directory.")
+            return
 
-      # Create HDF5 file for saving
-      print('Creating HDF5 file....')
-      with h5py.File(output_file, 'a') as h5f:
-          if "input_images" not in h5f:
-            input_dataset = h5f.create_dataset(
-                "input_images",
-                shape=(0, 3, 1025, 89),  # Initially empty along the first dimension
-                maxshape=(None, 3, 1025, 89),  # Unlimited along the first dimension
-                dtype=np.float32,
-                compression="gzip"
-            )
-          else:
-            input_dataset = h5f["input_images"]
+        # Load checkpoint to resume processing
+        if manual_checkpoint is not None:
+          start_batch_idx = manual_checkpoint
+        else:
+          start_batch_idx = load_checkpoint(checkpoint_file)
+        print(f"Resuming from batch index: {start_batch_idx}")
 
-          if "target_images" not in h5f:
-            target_dataset = h5f.create_dataset(
-                "target_images",
-                shape=(0, 3, 1025, 89),  # Initially empty along the first dimension
-                maxshape=(None, 3, 1025, 89),  # Unlimited along the first dimension
-                dtype=np.float32,
-                compression="gzip"
-            )
-          else:
-            target_dataset = h5f["target_images"]
+        # Create HDF5 file for saving
+        print('Creating HDF5 file....')
+        with h5py.File(output_file, 'a') as h5f:
+            if "input_images" not in h5f:
+              input_dataset = h5f.create_dataset(
+                  "input_images",
+                  shape=(0, 3, 1025, 89),  # Initially empty along the first dimension
+                  maxshape=(None, 3, 1025, 89),  # Unlimited along the first dimension
+                  dtype=np.float32,
+                  compression="gzip"
+              )
+            else:
+              input_dataset = h5f["input_images"]
 
-          print('checking for file existance....')
-          check_file_exists(output_file)
-          print('processing batches...')
-          # Process in batches
-          for i in tqdm(range(start_batch_idx, total_files, batch_size), desc="Processing batches", unit="batch"):
-              batch_files = wav_files[i:i + batch_size]
-              noise_files = random.sample(noise_wav_files, batch_size)
+            if "target_images" not in h5f:
+              target_dataset = h5f.create_dataset(
+                  "target_images",
+                  shape=(0, 3, 1025, 89),  # Initially empty along the first dimension
+                  maxshape=(None, 3, 1025, 89),  # Unlimited along the first dimension
+                  dtype=np.float32,
+                  compression="gzip"
+              )
+            else:
+              target_dataset = h5f["target_images"]
 
-              # Initialize lists to store input and target images for the batch
-              input_images = []
-              target_images = []
+            print('checking for file existance....')
+            check_file_exists(output_file)
+            print('processing batches...')
+            # Process in batches
+            for i in tqdm(range(start_batch_idx, total_files, batch_size), desc="Processing batches", unit="batch"):
+                batch_files = wav_files[i:i + batch_size]
+                noise_files = random.sample(noise_wav_files, batch_size)
 
-              #print(np.array([batch_files, noise_files]).T[:5])
+                # Initialize lists to store input and target images for the batch
+                input_images = []
+                target_images = []
 
-              # Process files in parallel
-              if process_pool:
-                with ProcessPoolExecutor() as executor:
-                    futures = [
-                      executor.submit(process_file, audio_file, noise_file, background_noise_level, random_noise_level, SNRdB) 
-                      for audio_file, noise_file in zip(batch_files, noise_files)
-                      ]
-                    results = [future.result() for future in futures if future.result() is not None]
+                #print(np.array([batch_files, noise_files]).T[:5])
 
-                # Collect batch results
-                for input_image, target_image in results:
-                    input_images.append(input_image)
-                    target_images.append(target_image)
+                # Process files in parallel
+                if process_pool:
+                  with ProcessPoolExecutor() as executor:
+                      futures = [
+                        executor.submit(process_file, audio_file, noise_file, background_noise_level, random_noise_level, SNRdB) 
+                        for audio_file, noise_file in zip(batch_files, noise_files)
+                        ]
+                      results = [future.result() for future in futures if future.result() is not None]
 
-              # process files as a loop
-              else:
-                for audio_file, noise_file in zip(batch_files, noise_files):
-                  output = process_file(audio_file, noise_file, background_noise_level, random_noise_level, SNRdB)
+                  # Collect batch results
+                  for input_image, target_image in results:
+                      input_images.append(input_image)
+                      target_images.append(target_image)
 
-                  # collect batch results
-                  if output is not None:
-                    input_image, target_image = output
-                    input_images.append(input_image)
-                    target_images.append(target_image)
-                  else:
-                    print(f"Error processing {audio_file}")
+                # process files as a loop
+                else:
+                  for audio_file, noise_file in zip(batch_files, noise_files):
+                    output = process_file(audio_file, noise_file, background_noise_level, random_noise_level, SNRdB)
 
-              # Append batch to datasets
-              input_dataset.resize(input_dataset.shape[0] + len(input_images), axis=0)
-              target_dataset.resize(target_dataset.shape[0] + len(target_images), axis=0)
-              input_dataset[-len(input_images):] = np.stack(input_images)
-              target_dataset[-len(target_images):] = np.stack(target_images)
+                    # collect batch results
+                    if output is not None:
+                      input_image, target_image = output
+                      input_images.append(input_image)
+                      target_images.append(target_image)
+                    else:
+                      print(f"Error processing {audio_file}")
 
-              if verbose:
-                print('input dataset shape:', input_dataset.shape)
-                print('target dataset shape:', target_dataset.shape)
-              # Save checkpoint after each batch
-              save_checkpoint(checkpoint_file, i + batch_size)
+                # Append batch to datasets
+                input_dataset.resize(input_dataset.shape[0] + len(input_images), axis=0)
+                target_dataset.resize(target_dataset.shape[0] + len(target_images), axis=0)
+                input_dataset[-len(input_images):] = np.stack(input_images)
+                target_dataset[-len(target_images):] = np.stack(target_images)
+
+                if verbose:
+                  print('input dataset shape:', input_dataset.shape)
+                  print('target dataset shape:', target_dataset.shape)
+                # Save checkpoint after each batch
+                save_checkpoint(checkpoint_file, i + batch_size)
+
+                if os.path.exists(output_file):
+                  current_size = os.path.getsize(output_file)
+                  if current_size >= max_file_size_bytes:
+                    LOGIC = False
+
+                if i > checkpoint_file_size:
+                  break
 
     except Exception as e:
         # Log the exception
