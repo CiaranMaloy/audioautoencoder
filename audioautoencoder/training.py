dab233fde00fc6d5a59df1a8f96c7e0eef73718a
Ciaran Maloy <1ciaran.maloy@gmail.com>
2025-01-24 10:21:01 +0000

    problem with loss

--- audioautoencoder/training.py
+++ audioautoencoder/training.py
@@ -104,6 +104,8 @@ def train_model(model, train_loader, val_loader, criterion, optimizer, scheduler
             if verbose:
               print('training model')
             outputs = model(noisy_imgs)
+            print(np.shape(outputs))
+            print(np.shape(clean_imgs))
             loss = criterion(outputs, clean_imgs)
             loss.backward()
             optimizer.step()
