da778650ee5342b33ceeb6d7989356dae886edbb
Ciaran Maloy <1ciaran.maloy@gmail.com>
2025-01-31 15:01:21 +0000

    added accumulating gradients to simulate larger batch size

--- audioautoencoder/training.py
+++ audioautoencoder/training.py
@@ -165,7 +165,7 @@ def train_model(model,
             #benchark_loss += criterion(noisy_imgs, clean_imgs).item()
             #recon_loss += r_loss.item()
             ref_loss += criterion(noisy_imgs[:, 0:1, :, :], clean_imgs[:, 0:1, :, :]).item()
-            progress_bar.set_postfix(loss=f"loss: {(running_loss) / (progress_bar.n + 1):.4f}, ref:{(ref_loss) / (progress_bar.n + 1):.4f}")
+            progress_bar.set_postfix(loss=f"loss: {(running_loss * accumulation_steps) / (progress_bar.n + 1):.4f}, ref:{(ref_loss * accumulation_steps) / (progress_bar.n + 1):.4f}")
             #progress_bar.set_postfix(loss=f"{running_loss / (progress_bar.n + 1):.4f}, bl:{benchark_loss / (progress_bar.n + 1):.4f}")
             
             # i++
