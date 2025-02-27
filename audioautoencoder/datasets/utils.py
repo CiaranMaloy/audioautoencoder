6cb6be5a1b113b0df3913df8337fce0c51208f4b
Ciaran Maloy <1ciaran.maloy@gmail.com>
2025-02-27 14:35:52 +0000

    updated adding ChannelDatasetLoader

--- audioautoencoder/datasets/utils.py
+++ audioautoencoder/datasets/utils.py
@@ -92,6 +92,45 @@ class ScalerDatasetLoader:
         self.train_dataset, _ = random_split(self.train_dataset, [int(subsample * len(self.train_dataset)), len(self.train_dataset) - int(subsample * len(self.train_dataset))], generator=split_rng)
         self.val_dataset, _ = random_split(self.val_dataset, [int(subsample * len(self.val_dataset)), len(self.val_dataset) - int(subsample * len(self.val_dataset))], generator=split_rng)
 
+class ChannelDatasetLoader:
+    def __init__(self, dataset_path, scalers, output_time_length=175, channels=1, snr_db=None, subset=False, batch_size=32):
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.batch_size = batch_size
+        self.num_workers = 0
+        self.subset = subset
+        self.snr_db = snr_db
+        self.dataset_path = dataset_path
+        self.scalers = scalers
+        self.output_time_length = output_time_length
+        self.channels = channels
+        self._initialize_dataset()
+    
+    def _initialize_dataset(self):
+        torch.manual_seed(42)
+        split_rng = torch.Generator().manual_seed(42)
+
+        dataset = HDF5Dataset_bandchannels(self.dataset_path, self.scalers, output_time_length=self.output_time_length, channels=self.channels)
+    
+        total_size = len(dataset)
+        train_size = int(0.8 * total_size)
+        val_size = total_size - train_size
+        self.train_dataset, self.val_dataset = random_split(dataset, [train_size, val_size], generator=split_rng)
+        
+        if self.subset:
+            self._apply_subset(split_rng)
+        
+        self.train_loader = DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=True, collate_fn=custom_collate_fn)
+        self.val_loader = DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=self.num_workers, pin_memory=True, collate_fn=custom_collate_fn)
+        
+        print(f"Training set size: {len(self.train_dataset)}")
+        print(f"Validation set size: {len(self.val_dataset)}")
+    
+    def _apply_subset(self, split_rng):
+        subsample = 0.35
+        self.train_dataset, _ = random_split(self.train_dataset, [int(subsample * len(self.train_dataset)), len(self.train_dataset) - int(subsample * len(self.train_dataset))], generator=split_rng)
+        self.val_dataset, _ = random_split(self.val_dataset, [int(subsample * len(self.val_dataset)), len(self.val_dataset) - int(subsample * len(self.val_dataset))], generator=split_rng)
+
+
 import h5py
 import numpy as np
 from sklearn.preprocessing import StandardScaler, MinMaxScaler, RobustScaler, MaxAbsScaler
