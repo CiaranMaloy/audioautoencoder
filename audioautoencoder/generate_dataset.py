09459d572a98f79142a834fd3a49a0738473a45d
Ciaran Maloy <1ciaran.maloy@gmail.com>
2025-03-18 07:03:12 +0000

    removed input spectrogram data from h5 reconstruction and diffusion dataloader

--- audioautoencoder/generate_dataset.py
+++ audioautoencoder/generate_dataset.py
@@ -516,6 +516,8 @@ def combine_h5_files_clean(h5_folder_path, output_folder_path, max_file_size_gb=
     h5_files = sorted(
         [os.path.join(h5_folder_path, f) for f in os.listdir(h5_folder_path) if f.endswith(".h5")]
     )
+    np.random.shuffle(np.array(h5_files))
+    print(h5_files[0:10])
     
     if not h5_files:
         print("No HDF5 files found in directory.")
